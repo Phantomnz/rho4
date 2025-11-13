ebd82777d7a73af0e1a6702995687c7137696f9f
Martynas Mickevicius <1mmickeviucius@gmail.com>
2025-11-13 14:47:13 +0000

    pre firmware linkage

--- optional/PWMTimer.hpp
+++ optional/PWMTimer.hpp
@@ -4,5 +4,5 @@
 class PWMTimer {
 public:
     PWMTimer(); // Constructor sets up the hardware
-    void setDutyCycle(uint8_t duty); // Sets the output
+    void setDutyCycle(uint8_t duty); // Sets the output of the PWM (0-255)
 };
